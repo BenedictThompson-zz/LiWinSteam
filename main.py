691b5f75ee038b57861fa4faced8e0cd9c1651e3
Ben Thompson <thompson.benedictjames@gmail.com>
2016-12-27 18:46:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import tkSimpleDialog
 import tarfile
 import Tkinter
 import time
-import os
+import subprocess
 import os.path
 import tkFileDialog
 root = Tkinter.Tk()
 root.withdraw()
 
-if not os.path.isfile("steamcmd.sh"): 
+if os.path.isfile("steamcmd.sh"): 
     tkMessageBox.showinfo("First Time Setup", "Downloading and extracting steamcmd. Please wait.")
     urllib.urlretrieve ("https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz", "steamcmd.tar.gz")
     time.sleep(0.5)
@@ -23,4 +23,9 @@ appid = tkSimpleDialog.askinteger("Enter App Id", "Enter the app id for the app 
 dir_opt = {}
 dir_opt['title'] = 'Please select a download location'
 installdir = tkFileDialog.askdirectory(**dir_opt)
-os.system("sudo ./steamcmd.sh +@sSteamCmdForcePlatformType windows " + "+login " + username + " " + password + " +force_install_dir '" + installdir + "' " +  "+app_update " + str(appid) + " -validate"+ " +quit")
+import shlex
+from subprocess import Popen, PIPE
+
+os.startfile(r"steamcmd.exe +@sSteamCmdForcePlatformType windows " + "+login " + username + " " + password + " +force_install_dir '" + installdir + "' " +  "+app_update " + str(appid) + " -validate"+ " +quit")
+
+#replace with ./steamcmd.sh
